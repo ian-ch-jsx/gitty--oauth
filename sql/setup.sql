04f63cc9576698995de7b4d9887555844de403ce
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-31 09:22:51 -0700

    initiated with alchemy bootstrap and setup SQL tables

---
+++ sql/setup.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+
+CREATE TABLE users (
+id BIGINT GENERATED ALWAYS AS PRIMARY KEY,
+email TEXT NOT NULL,
+password_hash TEXT NOT NULL,
+);
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS PRIMARY KEY,
+    post VARCHAR(255)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+INSERT INTO
+    posts (post)
+VALUES
+    ('I wish I could have, like, 400 kittens.'),
+    ('We only have one more week of instruction and I am gonna vomit!');
