60a8758557aadd5a243079d494c2dcc1dbeb9a83
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-31 11:14:45 -0700

    controller written for oauth redirect on github
    controller written
    test green after fixing accidental nested tests
    fixed syntax errors on sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS github_users CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE github_users (
@@ -9,14 +9,14 @@ CREATE TABLE github_users (
 );
 
 CREATE TABLE posts (
-    id BIGINT GENERATED ALWAYS AS PRIMARY KEY,
-    post VARCHAR(255)
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    post VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
 INSERT INTO
     posts (post)
 VALUES
     ('I wish I could have, like, 400 kittens.'),
-    (`We only have one more week of instruction and I'm gonna vomit!`),
-    (`Don't get excited, Trump, you're preemtively banned here, too`);
+    ('We only have one more week of instruction and Im gonna vomit!'),
+    ('Dont get excited, Trump, youre preemtively banned here, too');
