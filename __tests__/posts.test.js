05155c7bfefd7b606bead352681ce8f5543c2e14
Ian Christiansen <ianc.songdog@gmail.com>
2022-03-31 16:47:48 -0700

    wrote test for creating a post

---
+++ __tests__/posts.test.js
@@ -0,0 +1,37 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const GithubUser = require('../lib/models/GithubUser');
+
+describe('gitty posts routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('allows an authorized user to create posts', async () => {
+    await GithubUser.insert({
+      username: 'user',
+      photoUrl: 'http://picture.com/profile.png',
+    });
+
+    return request(app)
+      .post('api/v1/posts')
+      .send({ post: 'I like turtles.' })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          post: 'I like turtles',
+          username: 'user',
+        });
+      });
+  });
+  it('allows an authorized user to view posts', async () => {
+    // NO USER
+    // USER
+  });
+});
